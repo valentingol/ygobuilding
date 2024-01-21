19be35c674671a89ff9f8fd624e9b0fcb74d3826
Valentin Goldité <valentin.goldite@gmail.com>
2024-01-21 20:41:55 +0100

    🚀 parse_args return arg namespace

--- evaluate/parser.py
+++ evaluate/parser.py
@@ -1,14 +1,25 @@
 # Copyright (c) 2024 Valentin Goldite. All Rights Reserved.
 """Parser for command line arguments."""
 import argparse
-from typing import Tuple
+from argparse import Namespace
 
 
-def parse_args() -> Tuple[str, int, str]:
+def parse_args() -> Namespace:
     """Parse command line arguments."""
     parser = argparse.ArgumentParser()
     parser.add_argument(
-        "-n", "--number", help="Number of hands to evaluate", default=100_000, type=int
+        "-n",
+        "--num_runs",
+        help="Number of hands to evaluate",
+        default=100_000,
+        type=int,
+    )
+    parser.add_argument(
+        "-c",
+        "--num_cards",
+        help="Number of cards in hand",
+        default=5,
+        type=int,
     )
     parser.add_argument(
         "--name", help="Deck name (same as evaluate/ sub-folder names)", type=str
@@ -17,4 +28,4 @@ def parse_args() -> Tuple[str, int, str]:
         "--configs", help="Configurations file", type=str, required=True
     )
     args = parser.parse_args()
-    return args.name, args.number, args.configs
+    return args
